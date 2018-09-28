a3be30e01677cb0a2c4ffe6da9cc6893fe986ac9
Cooper <myxiaoao@gmail.com>
2018-09-28 13:31:18 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# ThinkPHP5ValetDriver
-ThinkPHP5 Valet Driver
+# ThinkPHP5 Valet Driver
+
+拷贝 `ThinkPHP5ValetDriver.php` 文件到 `valet` 驱动文件夹 `~/.valet/Drivers` 。
+
+**备注：Mojave 10.14 驱动文件夹为：`~/.config/valet/Drivers`！！！**
+
+## License
+
+The Lumen framework is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT)
---
+++ ThinkPHP5ValetDriver.php
@@ -0,0 +1,53 @@
+<?php
+
+class ThinkPHP5ValetDriver extends ValetDriver
+{
+    /**
+     * Determine if the driver serves the request.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return bool
+     */
+    public function serves($sitePath, $siteName, $uri)
+    {
+        return file_exists($sitePath . '/thinkphp/base.php') &&
+        file_exists($sitePath . '/think');
+    }
+    /**
+     * Determine if the incoming request is for a static file.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string|false
+     */
+    public function isStaticFile($sitePath, $siteName, $uri)
+    {
+        if (file_exists($staticFilePath = $sitePath . '/public' . $uri)
+            && is_file($staticFilePath)) {
+            return $staticFilePath;
+        }
+
+        return false;
+    }
+    /**
+     * Get the fully resolved path to the application's front controller.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string
+     */
+    public function frontControllerPath($sitePath, $siteName, $uri)
+    {
+        $_SERVER['SCRIPT_FILENAME'] = $sitePath . '/public/index.php';
+        $_SERVER['SERVER_NAME']     = $_SERVER['HTTP_HOST'];
+        $_SERVER['SCRIPT_NAME']     = '/index.php';
+        $_SERVER['PHP_SELF']        = '/index.php';
+        $_GET['s']                  = $uri;
+
+        return $sitePath . '/public/index.php';
+    }
+}
